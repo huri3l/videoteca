3a682c7b5f81b9856e9cd8e643b25c68a8775ffc
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-17 09:23:25 -0300

    refactor: Updating the port and configuring cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+const port = 3333;
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
-app.listen(3030, () => {
-  console.log("⚡ Backend started at http://localhost:3030");
+app.listen(port, () => {
+  console.log(`⚡ Backend started at http://localhost:${port}`);
 });
