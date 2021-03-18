f04f405b0a96604b6a32c0b75c71f9397a7d474e
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-18 11:17:02 -0300

    feat(global.css): Reset CSS and global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import Header from "./components/Header";
 import VideoList from "./components/VideoList";
+import Footer from "./components/Footer";
+
+import "./global.css";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <VideoList />
+      <Footer />
     </div>
   );
 }
