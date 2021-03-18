ceb0ec766f6e8e20724577332c5eacf6072049b3
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-18 11:19:16 -0300

    feat(Footer): Including application footer

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>Study app developed with ❤ by @huri3l</span>
+    </div>
+  );
+}
