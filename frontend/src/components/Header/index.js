908991b4ee26b277292c88c3934ee9c16dc336f0
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-18 11:19:01 -0300

    feat(Header): Including application header

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>VIDEO LIST</h1>
+      <h3>List your favorite videos 🎬</h3>
+    </div>
+  );
+}
