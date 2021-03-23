ee0d41d168a6b2eea3e2901288710a51391c100c
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-22 23:42:37 -0300

    feat(Database): First configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://huriellopes:nFWeTozCwrwgWMQm@cluster0.msgtn.mongodb.net/videoteca?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 const app = express();
 const port = 3333;
