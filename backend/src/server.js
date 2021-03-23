c8a9cd696dbd57413c3413e359defea3d7be0b5c
Huriel Lopes <huriel-lopes@hotmail.com>
2021-03-23 00:47:01 -0300

    feat(Database): Initial config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3333;
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("📦 Connected to the database!"));
 
 app.use(cors());
 app.use(express.json());
